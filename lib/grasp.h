4b173ab3bf11e2927b78339aae2237f05e306a69
EliasHernandezAbreu <alu0101487137@ull.edu.es>
2024-04-01 17:08:11 +0100

    start grasp plus new test

---
+++ lib/grasp.h
@@ -0,0 +1,24 @@
+/**
+ * Diseño y Analisis de algoritmos
+ * Practica 7 - PMSP
+ *
+ * @author Elias Hernandez Abreu <alu0101487137@ull.edu.es>
+ * @since 20 mar 2024
+ *
+ * @file Represents the greedy PMSP algorithm
+ */
+
+#pragma once
+
+#include "algorithm.h"
+
+class GraspPMSP : public Algorithm {
+public:
+  /**
+   * Solves a PMSP problem
+   * @param probelm The PMSP probelm
+   * @returns The found solution to the problem
+   */
+  Solution solve(const Problem *problem) const override;
+};
+
---
+++ src/grasp.cc
@@ -0,0 +1,35 @@
+/**
+ * Diseño y Analisis de algoritmos
+ * Practica 7 - PMSP
+ *
+ * @author Elias Hernandez Abreu <alu0101487137@ull.edu.es>
+ * @since 20 mar 2024
+ *
+ * @file Represents the greedy PMSP algorithm
+ */
+
+#include <vector>
+
+#include "../lib/grasp.h"
+
+Solution GraspPMSP::solve(const Problem *problem) const {
+  Solution result(problem);
+  for (int task = 1; task <= problem->getTaskAmount(); task++) { // start in task 1 because task 0 is dumb
+    int best_machine = 0;
+    int best_position = 0;
+    int best_tct = 9999999;
+    for (int current_machine = 0; current_machine < problem->getMachineAmount(); current_machine++) {
+      std::vector<int> machine_tasks = result.getTasks(current_machine);
+      for (int position = 0; position < machine_tasks.size(); position++) {
+        int current_tct = result.testAddTaskTCT(current_machine, task, position);
+        if (current_tct > best_tct) continue;
+        best_tct = current_tct;
+        best_position = position;
+        best_machine = current_machine;
+      }
+    }
+    result.addTask(best_machine, task, best_position);
+  }
+  return result;
+}
+
